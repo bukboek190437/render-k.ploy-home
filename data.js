14046cffab9ff68edfd736dffc5355917d2b3518
bukboek190437 <adtration670@gmail.com>
2025-07-02 17:55:35 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1-360-_0",
+      "name": "รูปที่1 360 องศา_0",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.040942857311304,
+          "pitch": 0.4751024386193645,
+          "rotation": 1.5707963267948966,
+          "target": "1-2-360-_0"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-2-360-_0",
+      "name": "รูปที่2 360 องศา_0",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1425552906635001,
+          "pitch": 0.19534470854990715,
+          "rotation": 1.5707963267948966,
+          "target": "0-1-360-_0"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "บ้านคุณพลอยชั้น1",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
